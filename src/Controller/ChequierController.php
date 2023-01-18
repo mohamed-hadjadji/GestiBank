c348206b445df4a7dd1339bebd91a689cfa0a201
mohamed hadjadji <mohamed.hadjadji@laplateforme.io>
2023-01-19 00:17:40 +0100

    a jour

--- src/Controller/ChequierController.php
+++ src/Controller/ChequierController.php
@@ -11,10 +11,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('client/chequier')]
+
 class ChequierController extends AbstractController
 {
-    #[Route('/', name: 'app_chequier_index', methods: ['GET'])]
+    
+    #[Route('/client/chequier', name: 'app_chequier_index', methods: ['GET'])]
     public function index(ChequierRepository $chequierRepository): Response
     {
         return $this->render('chequier/index.html.twig', [
@@ -22,15 +23,15 @@ class ChequierController extends AbstractController
         ]);
     }
 
-    // #[Route('/', name: 'app_chequier_index', methods: ['GET'])]
-    // public function index(ChequierRepository $chequierRepository): Response
-    // {
-    //     return $this->render('chequier/index.html.twig', [
-    //         'chequiers' => $chequierRepository->findAll(),
-    //     ]);
-    // }
+    #[Route('/agent/chequier', name: 'app_chequier_indexA', methods: ['GET'])]
+    public function indexA(ChequierRepository $chequierRepository): Response
+    {
+        return $this->render('chequier/indexA.html.twig', [
+            'chequiers' => $chequierRepository->findBy(array("id_agent" => $this->getUser()->getId())),
+        ]);
+    }
 
-    #[Route('/new', name: 'app_chequier_new', methods: ['GET', 'POST'])]
+    #[Route('/client/chequier/new', name: 'app_chequier_new', methods: ['GET', 'POST'])]
     public function new(Request $request, ChequierRepository $chequierRepository, CompteRepository $compteRepository): Response
     {
         $chequier = new Chequier();
@@ -58,7 +59,7 @@ class ChequierController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_chequier_show', methods: ['GET'])]
+    #[Route('client/chequier/{id}', name: 'app_chequier_show', methods: ['GET'])]
     public function show(Chequier $chequier): Response
     {
         return $this->render('chequier/show.html.twig', [
@@ -66,25 +67,15 @@ class ChequierController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_chequier_edit', methods: ['GET', 'POST'])]
+    #[Route('agent/chequier/{id}/edit', name: 'app_chequier_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Chequier $chequier, ChequierRepository $chequierRepository): Response
     {
-        $form = $this->createForm(ChequierType::class, $chequier);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
+        $chequier->setEtat('Valider');
             $chequierRepository->save($chequier, true);
-
-            return $this->redirectToRoute('app_chequier_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('chequier/edit.html.twig', [
-            'chequier' => $chequier,
-            'form' => $form,
-        ]);
+            return $this->redirectToRoute('app_chequier_indexA', [], Response::HTTP_SEE_OTHER);
     }
 
-    #[Route('/{id}', name: 'app_chequier_delete', methods: ['POST'])]
+    #[Route('client/chequier/{id}', name: 'app_chequier_delete', methods: ['POST'])]
     public function delete(Request $request, Chequier $chequier, ChequierRepository $chequierRepository): Response
     {
         if ($this->isCsrfTokenValid('delete'.$chequier->getId(), $request->request->get('_token'))) {
